fd1abd5def05b232f2f73384f1ee2bf3335f45ee
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-24 14:18:13 -0600

    submit button enablement

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from '../../logo.svg';
 import './App.css';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {
+        username: null,
+        email: null,
+        tripType: null
+      }
+    }
+  }
+
+  loginUser = (loginInfo) => {
+    this.setState({userInfo: loginInfo})
+    console.log(loginInfo)
+    console.log(this.state)
+  }
+
+  render() {
+    return (
     <div>
       <Header />
-      <Login />
+      <Login loginUser={this.loginUser}/>
     </div>
-  )
+    )
+  }
 }
 
 export default App;

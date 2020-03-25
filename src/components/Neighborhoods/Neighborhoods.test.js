ece5939664ba2d3c81b1bd2e16f724aefcab18b8
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-25 15:57:08 -0600

    neighborhood card test

--- src/components/Neighborhoods/Neighborhoods.test.js
+++ src/components/Neighborhoods/Neighborhoods.test.js
@@ -1,13 +1,22 @@
-// import React from 'react';
-// import Neighborhoods from './Neighborhoods';
-// import { render } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-//
-// describe('Neighborhoods', () => {
-//   it('renders neighborhood information to each card', () => {
-//     const { getByText } = render(
-//       <Neighborhoods
-//
-//     )
-//   })
-// })
+import React from 'react';
+import Neighborhoods from './Neighborhoods.js';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Neighborhoods', () => {
+  it('renders neighborhood information to each card', () => {
+    const { getByText } = render(
+      <Neighborhoods
+        name={"Lower Highlands"}
+        shorthand={"LoHi"}
+        about={"LoHi is the best neighborhood!"}
+        />);
+
+    const nameEl = getByText("Lower Highlands");
+    const shorthandEl = getByText("LoHi");
+    const aboutEl = getByText("LoHi is the best neighborhood!");
+    expect(nameEl).toBeInTheDocument();
+    expect(shorthandEl).toBeInTheDocument();
+    expect(aboutEl).toBeInTheDocument();
+  });
+});

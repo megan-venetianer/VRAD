1451ff11d5523e543e6e86f3e589576238eb3ca4
monstaro <monstaro32@gmail.com>
2020-03-26 17:52:25 -0600

    Creates route for area id

--- src/components/Neighborhoods/Neighborhoods.js
+++ src/components/Neighborhoods/Neighborhoods.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './Neighborhoods.css';
 
-const Neighborhoods = ({ name, about, shorthand }) => {
+const Neighborhoods = ({ name, about, shorthand, id }) => {
+  console.log(id)
   return (
     <div className='neighborhood-card'>
       <h2 className='neighborhood-header'>{name} ({shorthand})</h2>
       <p>{about}</p>
-      <button className='listing-btn'>View Listings</button>
+      <button className='listing-btn'>
+        <Link to="/areas/:id">
+          View Listings
+        </Link>
+      </button>
     </div>
   )
 }

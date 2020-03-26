54eac23437dea1c1181f7b0e83db0fa2318cd9c9
monstaro <monstaro32@gmail.com>
2020-03-25 20:45:53 -0600

    Begins logout testing, stuck on submit test

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Login from './Login';
+import '@testing-library/jest-dom';
+
+
+describe("Login", () => {
+  it('logs a user in after they enter all their info', () => {
+    const logIn = jest.fn()
+    const { getByText, getByPlaceholderText } = render(
+      <Login 
+        onClick={logIn}
+      />
+       
+       )
+    fireEvent.change(getByPlaceholderText('username'), {
+      target: {value: 'mockedUser'}
+    })
+    fireEvent.change(getByPlaceholderText('email address'), {
+      target: {value: 'mocked@email.com'}
+    })
+    fireEvent.change(getByPlaceholderText('listing type'), {
+      target: {value: 'Vacation'}
+    })
+    fireEvent.change(getByPlaceholderText('listing type'), {
+      target: {value: 'Business'}
+    },
+      getByText('Submit'), {
+        target: {disabled: false}
+      }
+    )
+    // console.log(getByText('Submit'))
+    fireEvent.click(getByText('Submit'))
+      // expect(logIn).toHaveBeenCalledTimes(1)
+    
+  })
+})

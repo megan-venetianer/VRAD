5ef10f5528008b0723f95f24fcbb10515fc42432
monstaro <monstaro32@gmail.com>
2020-03-26 21:23:29 -0600

    reformats back into functional component

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -3,47 +3,14 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 
-class Listings extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      listings: null
-    }
-  }
-  componentDidMount = () => {
-    fetch('http://localhost:3001/api/v1/areas/' + this.props.listId)
-      .then(response => response.json())
-      .then(data => {
-        const promises = data.listings.map(listing => {
-          return fetch('http://localhost:3001' + listing)
-          .then(res => res.json())
-          .then(data => data)
-        })
-        return Promise.all(promises)
-      })
-      .then(listings => {
-        this.setState({listings})
-      })
-      .catch(err => console.log(err.msg))
-
-  }
-  render() {
-    if (this.state.listings) {
-      return (
-        <div>
-          <h2>
-            {/* {console.log(this.state.listings)} */}
-            {this.state.listings.map(listing => listing.name)}
-          </h2>
-        </div>
-        )
-    } else {
-      return (
-        'Loading...'
-      )
-    }
-    
-  } 
+const Listings = ({listings}) => {
+console.log(listings)
+  return (
+    <section className="listing">
+      <h1>{listings.name}</h1>
+      <h2>{listings.address.street}</h2>
+    </section>
+  )
 }
 
 export default Listings

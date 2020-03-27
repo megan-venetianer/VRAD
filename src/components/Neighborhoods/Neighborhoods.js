e3a077737227e01e41a18641000f0f9e96424e40
monstaro <monstaro32@gmail.com>
2020-03-26 18:53:52 -0600

    adds router Link to neighborhoods

--- src/components/Neighborhoods/Neighborhoods.js
+++ src/components/Neighborhoods/Neighborhoods.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import './Neighborhoods.css';
 
 const Neighborhoods = ({ name, about, shorthand, id }) => {
-  console.log(id)
+
   return (
     <div className='neighborhood-card'>
       <h2 className='neighborhood-header'>{name} ({shorthand})</h2>
       <p>{about}</p>
       <button className='listing-btn'>
-        <Link to="/areas/:id">
+        <Link to={`areas/${id}`}>
           View Listings
         </Link>
       </button>

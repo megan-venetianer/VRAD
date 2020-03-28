65ba3895bd6d407c12683a0cdce929ca70d994ee
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-28 12:33:05 -0600

    Added Router to Neighborhoods test

--- src/components/Neighborhoods/Neighborhoods.test.js
+++ src/components/Neighborhoods/Neighborhoods.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Neighborhoods from './Neighborhoods.js';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import '@testing-library/jest-dom';
 
 describe('Neighborhoods', () => {
   it('renders neighborhood information to each card', () => {
     const { getByText } = render(
-      <Neighborhoods
+      <Router><Neighborhoods
         name={"Lower Highlands"}
         shorthand={"LoHi"}
         about={"Best neighborhood!"}
-        />);
+        /></Router>);
 
     const nameEl = getByText("Lower Highlands (LoHi)");
     const aboutEl = getByText("Best neighborhood!");

f6449a954123f2f24194885d1a40b262a34a8539
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-29 13:48:48 -0600

    trying to debug listings rendering

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ListingCard = ({ name, address, bathrooms, bedrooms, cost, features }) => {
-  console.log(name)
+
   return (
     <div>
       <p>{name}</p>
--- src/components/ListingCardContainer/ListingCardContainer.js
+++ src/components/ListingCardContainer/ListingCardContainer.js
@@ -5,23 +5,23 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ListingCardContainer = ({ username, tripType, currentListing }) => {
-  console.log(currentListing)
-  return (
-    <section data-testId="listingCardContainer" className="listing-card-container">
-      <h2 data-testid="neighborhoodHeader" className='greeting'>Hello {username.toUpperCase()}, you are viewing {tripType.toUpperCase()} rentals.
-      </h2>
-        <ListingCard
-          id={currentListing.listing_id}
-          key={currentListing.listing_id}
-          name={currentListing.name}
-          address={currentListing.address}
-          bathrooms={currentListing.details.baths}
-          bedrooms={currentListing.details.beds}
-          cost={currentListing.details.cost_per_night}
-          features={currentListing.details.features}
-        />
-    </section>
-  )
-}
+    return(
+      <section data-testId="listingCardContainer" className="listing-card-container">
+        <h2 data-testid="neighborhoodHeader" className='greeting'>Hello {this.props.username.toUpperCase()}, you are viewing {this.props.tripType.toUpperCase()} rentals.
+        </h2>
+          <ListingCard
+            id={currentListing.listing_id}
+            key={currentListing.listing_id}
+            name={currentListing.name}
+            address={currentListing.address}
+            bathrooms={currentListing.details.baths}
+            bedrooms={currentListing.details.beds}
+            cost={currentListing.details.cost_per_night}
+            features={currentListing.details.features}
+          />
+      </section>
+    )
+  }
+
 
 export default ListingCardContainer

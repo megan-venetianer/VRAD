17dd31c17ca92090b16096fd746d034899e9911f
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-29 15:12:55 -0600

    basic format of listing details card

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -6,9 +6,14 @@ import PropTypes from 'prop-types';
 const ListingCard = ({ id, name, address, bathrooms, bedrooms, cost, features }) => {
 
   return (
-    <div>
-      <p>{id}</p>
-      <p>{name}</p>
+    <div className="listing-details-card">
+      <h1 className="listing-details-header">{name}</h1>
+      <h3>Bedrooms: {bedrooms}</h3>
+      <h3>Bathrooms: {bathrooms}</h3>
+      {features.forEach(feature =>
+        <ul>feature</ul>
+      )}
+      <h2>Price per night: ${cost}</h2>
     </div>
   )
 }
--- src/components/ListingCardContainer/ListingCardContainer.js
+++ src/components/ListingCardContainer/ListingCardContainer.js
@@ -7,26 +7,24 @@ import PropTypes from 'prop-types';
 const ListingCardContainer = ({ username, tripType, currentListing }) => {
   if (username) {
     return(
-      <section>
-        <h2>
-        HELLO
+      <section data-testid="listingCardContainer" className="listing-card-container">
+        <h2 data-testid="neighborhoodHeader" className='greeting'>Hello {username.toUpperCase()}, you are viewing {tripType.toUpperCase()} rentals.
         </h2>
         <ListingCard
           id={currentListing.listing_id}
           key={currentListing.listing_id}
           name={currentListing.name}
+          address={currentListing.address}
+          bathrooms={currentListing.details.baths}
+          bedrooms={currentListing.details.beds}
+          cost={currentListing.details.cost_per_night}
+          features={currentListing.details.features}
         />
-        {/*// <ListingCard
-        // id={currentListing.listing_id}
-        // key={currentListing.listing_id}
-        // name={currentListing.name}
-        // address={currentListing.address}
-        // />*/}
       </section>
     )
-  } else {
-    return 'hioo'
-  }
+    } else {
+      return 'error'
+    }
   }
 export default ListingCardContainer
 

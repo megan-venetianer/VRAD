67d67e7dd879df20b867b016ced4c95081d44701
monstaro <monstaro32@gmail.com>
2020-03-28 19:36:44 -0600

    tests

--- src/components/Listings/Listings.test.js
+++ src/components/Listings/Listings.test.js
@@ -17,3 +17,18 @@ describe('Listings', () => {
     expect(nameEl).toBeInTheDocument();
   });
 });
+
+fetch('http://localhost:3001/api/v1/areas/' + this.props.listId)
+.then(response => response.json())
+.then(data => {
+  const promises = data.listings.map(listing => {
+    return fetch('http://localhost:3001' + listing)
+    .then(res => res.json())
+    .then(data => data)
+  })
+  return Promise.all(promises)
+})
+.then(listings => {
+  this.setState({listings})
+})
+.catch(err => err.message)

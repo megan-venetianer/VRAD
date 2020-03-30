054cd7b24dfce8b6a7a0071aa6d403e8f1255cfb
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-29 21:03:31 -0600

    listing card styling

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -6,7 +6,6 @@ import like from '../../images/like.svg'
 
 const ListingCard = ({ id, name, address, bathrooms, bedrooms, cost, features }) => {
 
-<<<<<<< HEAD
   return (
     <div className="listing-details-card">
       <div className="listing-header">
@@ -16,21 +15,6 @@ const ListingCard = ({ id, name, address, bathrooms, bedrooms, cost, features })
           <img className="favorite-img" src={like} alt="add to favorites" />
         </button>
       </div>
-=======
-const ListingCard = ({ id, name, address, bathrooms, bedrooms, cost, features, addToFavorites, isFavorited }) => {
-  const getFaveButton = () => {
-    if (isFavorited) {
-      return 'Remove From Favorites'
-    } else {
-      return 'Add To Favorites'
-    }
-}
-
-  return (
-    <div className="listing-details-card">
-      <h1 className="listing-details-header">{name}</h1>
-      <button onClick={() => addToFavorites(id)}className="favorite-btn">{getFaveButton()}</button>
->>>>>>> master
       <div className="listing-img-container">
         <img className="listing-details-img" src={`../../../images/${id}_a.jpg`} alt={name}/>
         <img className="listing-details-img" src={`../../../images/${id}_b.jpg`} alt={name}/>

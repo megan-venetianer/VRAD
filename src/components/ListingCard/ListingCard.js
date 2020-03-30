4f108e1bfa8698ccb59fd70575f0040617f03389
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-29 19:51:43 -0600

    styling listing cards

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -8,26 +8,32 @@ const ListingCard = ({ id, name, address, bathrooms, bedrooms, cost, features })
 
   return (
     <div className="listing-details-card">
-      <h1 className="listing-details-header">{name}</h1>
-      <button className="favorite-btn">
-        Add to Favorites
-        <img className="favorite-img" src={like} alt="add to favorites" />
-      </button>
+      <div className="listing-header">
+        <h1 className="listing-details-header">{name}</h1>
+        <button className="favorite-btn">
+          Add to Favorites
+          <img className="favorite-img" src={like} alt="add to favorites" />
+        </button>
+      </div>
       <div className="listing-img-container">
         <img className="listing-details-img" src={`../../../images/${id}_a.jpg`} alt={name}/>
         <img className="listing-details-img" src={`../../../images/${id}_b.jpg`} alt={name}/>
         <img className="listing-details-img" src={`../../../images/${id}_c.jpg`} alt={name}/>
       </div>
-      <h3>Bedrooms: {bedrooms}</h3>
-      <h3>Bathrooms: {bathrooms}</h3>
+      <div className="features-container">
       <h3>Features</h3>
       {features.map(feature => (
         <ul>
-          <li>{feature}</li>
+        <li>{feature}</li>
         </ul>
-        )
-      )}
+      )
+    )}
+    </div>
+    <div className="listing-details">
+      <h3>Bedrooms: {bedrooms}</h3>
+      <h3>Bathrooms: {bathrooms}</h3>
       <h2>Price per night: ${cost}</h2>
+    </div>
     </div>
   )
 }
